9a967734a04a12509061fcc59094926435c4cb28
IvanGomezGon <102017616+IvanGomezGon@users.noreply.github.com>
2022-12-27 23:13:02 +0100

    Add files via upload

---
+++ funcions.js
@@ -0,0 +1,13 @@
+
+function showhide(id){
+
+    if (document.getElementById) {
+
+        $( "#menu_seleccionado" ).load( "cine.html" );
+        divid.scrollIntoView({behavior:'smooth'});
+       
+      
+    } 
+    return false;
+}
+ 
