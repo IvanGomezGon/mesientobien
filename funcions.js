93264164ca87b5b7c6879b53442718426ac824ae
IvanGomezGon <102017616+IvanGomezGon@users.noreply.github.com>
2022-12-27 23:48:01 +0100

    option pops up when you select it from the menu

--- funcions.js
+++ funcions.js
@@ -2,8 +2,8 @@
 function showhide(id){
 
     if (document.getElementById) {
-
         $( "#menu_seleccionado" ).load( "cine.html" );
+        document.getElementById("menu_seleccionado").style.display = "block";
         document.getElementById("menu_seleccionado").scrollIntoView({behavior:'smooth'});
        
       
