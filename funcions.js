be70f9ebd3248fc2debb486ac1934dc83599af12
IvanGomezGon <102017616+IvanGomezGon@users.noreply.github.com>
2023-01-04 02:03:32 +0100

    interior_menu + favicon

--- funcions.js
+++ funcions.js
@@ -1,13 +1,33 @@
 
-async function showhide(id){
+async function showCine(id){
 
     if (document.getElementById) {
         await $( "#menu_seleccionado" ).load( "cine.html" );
         document.getElementById("menu_seleccionado").style.display = "block";
-        document.getElementById("menu_seleccionado").scrollIntoView({behavior:'smooth'});
-       
-      
+        document.getElementById("menu_seleccionado").scrollIntoView({behavior:'smooth'});          
     } 
     return false;
 }
+
+async function showInterior(id){
+
+    if (document.getElementById) {
+        await $( "#menu_seleccionado" ).load( "interior_design.html" );
+        document.getElementById("menu_seleccionado").style.display = "block";
+        document.getElementById("menu_seleccionado").scrollIntoView({behavior:'smooth'});          
+    } 
+    return false;
+}
+
+async function showhide(id){
+
+
+}
+
+$(document).ready(function(){
+    $(".house img").mouseover(function(){
+        console.log("que");
+        $("h2").css("display:block;");
+    });
+});
  
