46cd52d22e2af1afe722697e467b0814512e951f
IvanGomezGon <102017616+IvanGomezGon@users.noreply.github.com>
2023-01-04 02:54:06 +0100

    interior design menu height 100%

--- funcions.js
+++ funcions.js
@@ -13,6 +13,7 @@ async function showInterior(id){
 
     if (document.getElementById) {
         await $( "#menu_seleccionado" ).load( "interior_design.html" );
+        document.getElementById("menu_seleccionado").style.height = "100%";
         document.getElementById("menu_seleccionado").style.display = "block";
         document.getElementById("menu_seleccionado").scrollIntoView({behavior:'smooth'});          
     } 
